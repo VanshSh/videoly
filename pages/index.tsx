7758ec75c0ad14f65313a7d515fc22b57a212d92
Vansh Sharma <vansh@nowandme.com>
2022-07-04 17:21:26 +0530

    feat: :sparkles: Layout in the _app.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className='container' >
-      <h1 className='bg-red-700 p-8 text-xl text-white'>Videoly</h1>
+
     </div>
   )
 }
