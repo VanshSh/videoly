e34860880ad2b185ad352f4a76133b895c71126c
VanshSh <vansh@nowandme.com>
2022-07-17 12:37:35 +0530

    fix: :pencil2: API Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home = ({ videos }: IProps) => {
 }
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(`http://localhost:3001/api/post`)
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
 
   return {
     props: {
