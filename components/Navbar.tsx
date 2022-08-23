a4ce4436dc11d4189f90ff8dc36cf3ffe175d8a2
VanshSh <vanshsharma9354@gmail.com>
2022-08-23 22:14:19 +0530

    style: :lipstick: Add Logout button, Upload video button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,14 @@ import { IoMdAdd } from "react-icons/io";
 import tiktiklogo from '../utils/tiktik-logo.png'
 import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { createOrGetUser } from '../utils'
-
+import useAuthStore from '../store/authStore'
 
 
 const Navbar = () => {
-  const user = false
+
+  const { userProfile, addUser, removeUser } = useAuthStore()
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2'>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-2'>
       <Link href='/'>
         <div className='w-[100px] md:w-[130px] md:h-[30px] h-[38px]'>
           <Image src={tiktiklogo}
@@ -27,16 +28,37 @@ const Navbar = () => {
         Search
       </div>
       <div>
-        {user ? (
-          <div>LOGGEDIN</div>
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href='/upload'>
+              <button className='border-2 px-2 md:px-3 text-md gap-5 font-semibold flex items-center '>
+                <IoMdAdd className='text-xl' />
+                <span className='hidden md:block'>Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href='/'>
+                <>
+                  <Image width={40} height={40} className='rounded-full cursor-pointer' src={userProfile.image} alt='Profile photo' />
+                </>
+              </Link>
+            )}
+            <button type='button' className='px-2' onClick={() => {
+              googleLogout();
+              removeUser()
+            }
+            }>
+              <AiOutlineLogout color='red' fontSize={21} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => createOrGetUser(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log('error')}
           />
         )}
       </div>
-    </div>
+    </div >
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['randomuser.me'],
+        domains: ['randomuser.me', 'lh3.googleusercontent.com'],
     },
 }
 
